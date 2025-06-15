b79b83db562307e60c613ccebf5cbbb8f3c267e8
Siya10 <leyanesiyabonga00@gmail.com>
2025-06-15 14:08:26 +0200

    feat: added a new game creation page and a waiting lobby

---
+++ GameCreation.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPWordleClient
+{
+    partial class GameCreation: ContentPage
+    {
+        public GameCreation() 
+        {
+            InitializeComponent();
+            JoinGameBtn.WidthRequest = CreateGameBtn.Width;
+        }
+
+        private void OnCreateGame(Object sender, EventArgs e)
+        {
+
+        }
+
+        private async void OnJoinGame(Object sender, EventArgs e)
+        {
+            try
+            {
+                await DisplayPromptAsync("Join Game", "Enter the game code:", "Join", "Cancel", "Game Code", 6, keyboard: Keyboard.Numeric);
+                // This is where I'd be querrying the server to join the game but for now we just go straigh to the waiting room
+                await Shell.Current.GoToAsync("///Waiting");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error joining game: {ex.Message}");
+                await DisplayAlert("Error", "Failed to join the game. Please try again.", "OK");
+                return;
+            }
+        }
+    }
+}
---
+++ Waiting.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPWordleClient
+{
+    partial class Waiting: ContentPage
+    {
+        public Waiting()
+        {
+            InitializeComponent();
+        }
+        private void OnStartGame(Object sender, EventArgs e)
+        { 
+
+        }
+    }
+}
