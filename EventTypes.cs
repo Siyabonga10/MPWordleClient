36993e71d11949f8891ea301cebfe594dc5df670
Siya10 <leyanesiyabonga00@gmail.com>
2025-08-15 08:16:10 +0200

    feat: Added event types so that incomming events can be distinguished

---
+++ EventTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPWordleClient
+{
+    public static class EventTypes
+    {
+        public const string PlayersInGame = "PlayersInGame";
+        public const string PlayerJoined = "PlayerJoined";
+        public const string StartGame = "StartGame";
+        public const string WinnerUpdate = "WinnerUpdate";
+    }
+}
